924336e2de6f97457936c44e5d8075c002be4ab1
navjotsingh06sn <navjotsingh06sn@gmail.com>
2026-07-30 09:40:34 +0530

    DBMS

---
+++ Day09.py
@@ -0,0 +1,46 @@
+# DBMS
+# Data is collection of facts,figures, observation or information.
+# Eg='Satnam'
+# Data Base= It's an organized collection of data that stored and managed . so it can be easily accessed, update and retrieved.
+# Eg= Hotel management 
+
+#DBMS= It a software used to create ,manage and manipulate datbase
+#Eg= MYSql,Mongodb,oracledb
+# Why dbms and disadvantages
+# Data inconsistency
+# Security issue
+# Diffucult to back up 
+# Data isolation
+
+# Data base Architecture 
+
+# User view (data view) -->logical view(what type of data stored) --> Physical level(How the data is stored)
+
+# Type of data model
+
+# Hierarchical model :- Tree structure to store data.
+
+# Network Model :- It is like one record can be linked to many other records
+
+# Relation model :- It store data in the foem of tablets.
+
+# ER model :- It is agraphical reperesentation of entities aand relation between them in data dase
+
+# Schema - It is a structured framework or blueprint that organise information,concepts or data.
+
+# My Sql workbench- GUI (Graphical user interface) tool.
+# Defaut port number= 3306
+
+# Data base draw back if save locally 
+# Limite storage
+# We can't transer to another device
+
+
+# Flow Start
+
+# Data Storage --> Database Server --> URL/Host --> Connections --> Workbench --> queries --> cloud Database
+
+# Type of cloud -> AWS,Oracle Cloud (Oracle),GCP (Google cloud platform - Google),AZURe(amazon)
+
+# Primary key :- uniquely identifies each row i a table
+# Foregin key :-It is a field that link two tables together .
