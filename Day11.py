608208bce21eaa0fc21ccf53c2a797208942a77c
Satnam-dev <Satnamkumar.cse@gmail.com>
2026-08-03 11:01:53 +0530

    MongoDB

---
+++ Day11.py
@@ -0,0 +1,49 @@
+# MongoDB:- It is a NoSqL database that store data in key value pairs insted of row and tables.
+# Records = Documents
+# localhost:27017
+# React port = 5173
+
+# Prerequistie:- Something you need to know before learning or doing something else.
+# To store data on Cloud in mongoDB = Atlas
+# To store data on Locally in mongoDB = Compass
+# Step 1 
+# Open cluster in MongoDB Atlas and press on connect 
+
+# Step 2
+# select compass and copy that link
+# MongoBd URL :- mongodb+srv://satnamnv11:<db_password>@cluster0.qokzlw0.mongodb.net/
+
+# Step 3 
+# Create a env file in vs studio like MongoDB.env and paste link and start modifying it.
+
+# Step 4  Add Database in the end Rbu-Management (Can be anything)
+# Rbu-Management
+# mongodb+srv://satnamnv11:<db_password>@cluster0.qokzlw0.mongodb.net/Rbu-Management
+
+# Step 3 Use for security 
+# ?retryWrites=true&w=majority
+# mongodb+srv://satnamnv11:<db_password>@cluster0.qokzlw0.mongodb.net/Rbu-Management?retryWrites=true&w=majority
+
+# Step 4
+# If passward not shown then replace <db_password> to you current passward Admission100439
+# mongodb+srv://satnamnv11:Admission100439@cluster0.qokzlw0.mongodb.net/Rbu-Management?retryWrites=true&w=majority
+# We can't use special character in passward insted of char use like @=40% depend on the special character 
+
+# Step 5 download compass and paste the link made
+# mongodb+srv://satnamnv11:Admission100439@cluster0.qokzlw0.mongodb.net/Rbu-Management?retryWrites=true&w=majority
+
+
+# Use composs and create a new database and file you created add data like
+# /** 
+# * Paste one or more documents here
+# */
+# {
+#   "_id": {
+#     "$oid": "6a338b9cd99e14ed6dd544b3"
+#   },
+
+#   "Name":"Navjot",
+#   "Course":"Btech",
+#   "Roll No":3272
+# }
+
