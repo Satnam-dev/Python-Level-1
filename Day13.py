1f39b0c9cce485d14c9b3c27b4a262c7fd02316e
Satnam-dev <Satnamkumar.cse@gmail.com>
2026-08-06 09:47:11 +0530

    Node.js

---
+++ Day13.py
@@ -0,0 +1,32 @@
+# Download Node.js
+# 0.0.0.0/0 = This access list entry potentially allows access to all IPv4 addresses (Limited device connect)
+
+# An environment variable is a value stored otside yoour code that your application can read.
+# Enviroment variable sored in env file 
+
+# BackEnd
+# Server.js or index.js
+# Entry point of the Application 
+# Start the server
+# Connects middleware and routes (Middleware= Checkpoint before the route)
+
+# DB Connection
+# Conect your app to the data base (MongoDB ,MYSql
+
+# Model
+# Define the structire of data in database
+
+# Controller 
+# 1.Contain Bussiness logic
+# 2.Recieve request and send response
+
+# Routers
+# Define URLs and map them to controllers.
+
+# JWT = Jason web Token, It is used to verify user identity
+
+# PORT NUMBER:- It is an address for specific application on a computer 
+# Cors= Cross origin resource sharing ,It is security feature in browser that control whether one website can make request to another websites serveer.
+
+# Encrypt(hash)password before storing them in database passward= bcrypt.hash
+# Total Schema = Total router = total controllers
